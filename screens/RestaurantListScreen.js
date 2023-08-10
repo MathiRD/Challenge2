512fad715dee57d305ddc5d609944fdf348c5a09
matheusFabri <matheus.fabri@viannasempre.com.br>
2023-08-10 12:13:23 -0300

    feat: add dependencies zustand and tanstack query

--- screens/RestaurantListScreen.js
+++ screens/RestaurantListScreen.js
@@ -1,73 +1,75 @@
-import {View, Text, StyleSheet, Image, TouchableOpacity, FlatList} from 'react-native';
-import axios from 'axios';
-import React, { useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 
+const RestaurantListScreen = () => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-const RestaurantListScreen = ({ navigation }) => {
-    const [restaurants, setRestaurants] = useState([]);
-    const [loading, setLoading] = useState([true]);
-    
-    
-    
-    const navigateToDetals = () => {
-        navigation.navigate('RestaurantsDetails');
-    };
+  const navigation = useNavigation();
 
-    const NavigateToInitialScreen = () => {
-        navigation.navigate('InitialScreen')
-    }
+  const navigateToDetails = () => {
+    navigation.navigate("RestaurantsDetails");
+  };
 
-    useEffect(() => {
-        axios.get('https://8jcox47hg2.execute-api.us-east-2.amazonaws.com/dev')
-            .then(response => {
-                setRestaurants(response.data.body.restaurants);
-                setLoading(false);
-            })
-            .catch(error => {
-                console.error(error);
-                setLoading(false);
-            })
-    })
+  useEffect(() => {
+    axios
+      .get("https://8jcox47hg2.execute-api.us-east-2.amazonaws.com/dev")
+      .then((response) => {
+        setRestaurants(response.data.body.restaurants);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
+  });
 
-    return (
-        <View  style={styles.container}>
-            <View  style={styles.textProvisory}>
-                <Text style={styles.RestaurantCardText} >Hello</Text>
-                <TouchableOpacity onPress={NavigateToInitialScreen}>
-                    <Text style={styles.RestaurantCardText} >Go back</Text>
-                </TouchableOpacity>
+  return (
+    <View style={styles.container}>
+      <View style={styles.textProvisory}>
+        {/* <Text style={styles.RestaurantCardText}>Hello</Text> */}
+        <TouchableOpacity onPress={navigateToDetails}>
+          <Text style={styles.RestaurantCardText}></Text>
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        data={restaurants}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <Image source={{ uri: item.CoverImageUrl }} />
+            <View>
+              <Text style={styles.RestaurantCardText}>{item.name}</Text>
             </View>
-            <FlatList
-                data={restaurants}
-                keyExtractor={item => item.id.toString()}
-                renderItem={({ item }) => (
-                    <TouchableOpacity>
-                        <Image source={{ uri: item.CoverImageUrl }}/>
-                        <View>
-                            <Text style={styles.RestaurantCardText}>{item.name}</Text>
-                        </View>
-                        <FlatList
-                        />
-                    </TouchableOpacity>
-                )}
-            />
-        </View>
-    );
+          </TouchableOpacity>
+        )}
+      />
+      <FlatList />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#2c2c2e',
-        flex: 1,
-
-    },
-    textProvisory: {
-        marginTop: 100,
-        alignItems: 'center',
-    },
-    RestaurantCardText: {
-        color: '#fff'
-    }
+  container: {
+    backgroundColor: "#2c2c2e",
+    flex: 1,
+  },
+  textProvisory: {
+    marginTop: 100,
+    alignItems: "center",
+  },
+  RestaurantCardText: {
+    color: "#fff",
+  },
 });
 
 export default RestaurantListScreen;
