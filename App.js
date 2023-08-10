b8b0cbddb204ced8912496f00edcf30f823c194b
Rhayra <rhayra.fiorentin@gmail.com>
2023-08-10 11:01:51 -0300

    added the third screen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import InitialScreen from "./screens/InitialScreen";
-import RestaurantListScreen from "./screens/RestaurantListScreen";
+import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import InitialScreen from './screens/InitialScreen'
+import RestaurantListScreen from './screens/RestaurantListScreen'
+import DetailsScreen from './screens/DetailsScreen'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
@@ -18,16 +19,17 @@ export default function App() {
           name="restaurantListScreen"
           component={RestaurantListScreen}
         />
+        <Stack.Screen name="Detalhes" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
