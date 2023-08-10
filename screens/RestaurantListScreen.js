dae2e30e7cce7de0e20f580fa2a4faba8d27df0f
Matheus D <matheusdurigon1@gmail.com>
2023-08-10 14:31:06 -0300

    feat: Second screen almost done

--- screens/RestaurantListScreen.js
+++ screens/RestaurantListScreen.js
@@ -9,6 +9,7 @@ import {
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { ActivityIndicator } from "react-native";
 
 const RestaurantListScreen = () => {
   const [restaurants, setRestaurants] = useState([]);
@@ -17,7 +18,7 @@ const RestaurantListScreen = () => {
   const navigation = useNavigation();
 
   const navigateToDetails = () => {
-    navigation.navigate("RestaurantsDetails");
+    navigation.navigate("DetailsScreen");
   };
 
   useEffect(() => {
@@ -33,6 +34,10 @@ const RestaurantListScreen = () => {
       });
   });
 
+  if (loading) {
+    return < ActivityIndicator />
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.textProvisory}>
@@ -43,17 +48,24 @@ const RestaurantListScreen = () => {
       </View>
       <FlatList
         data={restaurants}
-        keyExtractor={(item) => item.id.toString()}
+        keyExtractor={item => item.id.toString()}
         renderItem={({ item }) => (
-          <TouchableOpacity>
-            <Image source={{ uri: item.CoverImageUrl }} />
-            <View>
-              <Text style={styles.RestaurantCardText}>{item.name}</Text>
+          <TouchableOpacity style={styles.card} onPress={() => navigateToDetails(item)}>
+            <Image source={{ uri: item.coverImageUrl }} style={styles.cardImage} resizeMode="cover" />
+            <View style={styles.cardInfo}>
+              <Text style={styles.cardTitle}>{item.name}</Text>
             </View>
+            <FlatList
+              data={item.menu}
+              keyExtractor={(menuItem, index) => `${item.id}-${index}`}
+              horizontal
+              renderItem={({ item: menuItem }) => (
+                <Image source={{ uri: menuItem.imageUrl }} style={styles.menuImage} />
+              )}
+            />
           </TouchableOpacity>
         )}
       />
-      <FlatList />
     </View>
   );
 };
@@ -70,6 +82,41 @@ const styles = StyleSheet.create({
   RestaurantCardText: {
     color: "#fff",
   },
+  card: {
+    width: 370,
+    height: 150,
+    flexShrink: 0,
+    marginVertical: 10,
+    marginHorizontal: 22,
+    borderRadius: 12,
+    backgroundColor: '#43ff64d9',
+    overflow: 'hidden',
+    elevation: 5
+  },  
+  cardImage: {
+    width: '100%',
+    height: '100%'
+  },
+  cardInfo: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    padding: 10,
+    borderBottomLeftRadius: 12,
+    borderBottomRightRadius: 12
+  },
+  cardTitle: {
+    color: '#fff',
+    elevation: 5,
+    lineHeight: 32
+
+  },
+  menuImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 8,
+    marginHorizontal: 5
+  }
 });
 
 export default RestaurantListScreen;
