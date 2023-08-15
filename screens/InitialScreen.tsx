670e4e2b7f874843fcb41f2d3980c7b199496b74
Matheus D <matheusdurigon1@gmail.com>
2023-08-15 17:11:56 -0300

    Fix: Button writing

--- screens/InitialScreen.tsx
+++ screens/InitialScreen.tsx
@@ -54,7 +54,7 @@ const InitialScreen = () => {
                 onClick={navegarParaRestaurantes}
                 style={styles.buttonInicial}
               >
-                Acess
+                Access
               </Button>
             </View>
           </BackGround>
